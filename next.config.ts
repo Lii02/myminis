2d8cca63b4d2d1beebf4138abe4f04de971251b8
Luke Inlow <lukeinlow@gmail.com>
2025-03-24 13:09:38 -0400

    Removed rewrites function

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
-	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: 'http://localhost:5000/api/:path*/',
-			},
-		];
-	},
 };
 
 export default nextConfig;
