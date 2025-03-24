13d235f09549b9c523e4ecffb47985bef6e7e14e
Luke Inlow <lukeinlow@gmail.com>
2025-03-24 14:33:08 -0400

    Created backend

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from 'next';
+import process from 'process';
 
 const nextConfig: NextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination:
+					process.env.NODE_ENV === 'development'
+						? 'http://localhost:5000/api/:path*'
+						: 'https://myminis.ue.r.appspot.com/api/:path*',
+			},
+		];
 	},
 };
 
